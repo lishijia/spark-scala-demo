232d5c11941aa76d5b6f345d575181da1b4dc462
lishijia <shijia.li@hotmail.com>
2018-10-21 20:33:44 +0800

    wordcount

--- src/main/scala/lishijia/spark/demo/wordcount/WordCount.scala
+++ src/main/scala/lishijia/spark/demo/wordcount/WordCount.scala
@@ -1,18 +1,13 @@
-package lishijia.spark.demo.wordcount;
+package lishijia.spark.demo.wordcount
 
-/**
-  * lishijia
-  *
-  */
+import org.apache.spark.{SparkConf, SparkContext}
+
 object WordCount {
 
   def main(args: Array[String]): Unit = {
-
     val conf = new SparkConf().setAppName("Spark WordCount")
     val sc = new SparkContext(conf)
-
     val lines = sc.textFile("hdfs://master201:9000/lishijia/input/the_man_of_property.txt")
-
     lines.flatMap(x=>x.split(" "))
       .map(x=>(x,1))
       .reduceByKey((x,y)=>x+y)
